3004e536ebd1fb36241be28a790c983e4be7b448
Ben Lubar <ben.lubar@gmail.com>
2015-08-19 14:58:30 -0500

    working (hopefully) version of the camxes-with-experimental-cmavo

---
+++ generate-cmavo.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+
+	"regexp"
+
+	"github.com/BenLubar/jbo/jbovlaste"
+)
+
+func main() {
+	var dict jbovlaste.Dictionary
+	err := xml.NewDecoder(os.Stdin).Decode(&dict)
+	if err != nil {
+		panic(err)
+	}
+
+	var selmaho []string
+	selmahoValsi := make(map[string][]string)
+
+	re := regexp.MustCompile(`\A[A-Zh]+`)
+
+	for i := range dict.Direction[0].Valsi {
+		valsi := &dict.Direction[0].Valsi[i]
+
+		if valsi.Type == "cmavo" || valsi.Type == "experimental cmavo" {
+			s := re.FindString(valsi.Selmaho)
+			if len(selmahoValsi[s]) == 0 {
+				selmaho = append(selmaho, s)
+			}
+			selmahoValsi[s] = append(selmahoValsi[s], valsi.Word)
+		}
+	}
+
+	sort.Sort(selmahoSort(selmaho))
+
+	for _, s := range selmaho {
+		fmt.Print(s, " = &cmavo expr:(")
+		for i, v := range selmahoValsi[s] {
+			if i != 0 {
+				fmt.Print(" /")
+			}
+			for j := range v {
+				if v[j] == '\'' {
+					fmt.Print(" h")
+				} else {
+					fmt.Print(" ", string(v[j]))
+				}
+			}
+		}
+		fmt.Print(" ) &post_word {return [\"", s, "\", _join(expr)];}\n\n")
+	}
+}
+
+var yhy = strings.NewReplacer("h", "'")
+
+type selmahoSort []string
+
+func (s selmahoSort) Len() int      { return len(s) }
+func (s selmahoSort) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+func (s selmahoSort) Less(i, j int) bool {
+	return strings.ToLower(yhy.Replace(s[i])) < strings.ToLower(yhy.Replace(s[j]))
+}
